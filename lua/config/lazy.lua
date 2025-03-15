7a3a4e3a14177519a4b95d0e468a10b76d5ac699
Guinn <eguinjonathan08@gmail.com>
2025-03-15 11:22:22 +0700

    added more lsp config and remove annoying notification

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,7 +21,6 @@ require("lazy").setup({
       colorscheme = "tokyonight-storm",
     } },
     { import = "lazyvim.plugins.extras.linting.eslint" },
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
