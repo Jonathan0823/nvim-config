c3899e86c8557c9b2ad6df4f45e9a90fc337bd6d
Jonathan0823 <eguinjonathan08@gmail.com>
2026-02-14 14:21:47 +0700

    feat(smear-cursor): add cursor burn effect

--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -23,17 +23,31 @@ return {
   },
 
   -- cursor animation
-  -- {
-  --   "sphamba/smear-cursor.nvim",
-  --   opts = {
-  --     smear_insert_mode = false,
-  --     stiffness = 0.8, -- 0.6      [0, 1]
-  --     trailing_stiffness = 0.5, -- 0.3      [0, 1]
-  --     distance_stop_animating = 0.5, -- 0.1      > 0j
-  --     cursor_color = "#d3cdc3",
-  --   },
-  -- },
-  --
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {
+      cursor_color = "#ff4000",
+      particles_enabled = true,
+      stiffness = 0.5,
+      trailing_stiffness = 0.2,
+      trailing_exponent = 5,
+      damping = 0.6,
+      gradient_exponent = 0,
+      gamma = 1,
+      never_draw_over_target = true, -- if you want to actually see under the cursor
+      hide_target_hack = true,    -- same
+      particle_spread = 1,
+      particles_per_second = 500,
+      particles_per_length = 50,
+      particle_max_lifetime = 800,
+      particle_max_initial_velocity = 20,
+      particle_velocity_from_cursor = 0.5,
+      particle_damping = 0.15,
+      particle_gravity = -50,
+      min_distance_emit_particles = 0,
+    },
+  },
+
   -- remove scroll animattion
   {
     "snacks.nvim",
